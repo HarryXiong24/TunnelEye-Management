abdf068451b233df941446a85523270dd9fe51c0
HarryXiong24 <haoweixiong@gmail.com>
2021-04-27 16:18:29 +0800

    0.1.2

--- src/components/ThemePicker/index.vue
+++ src/components/ThemePicker/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-color-picker
     v-model="theme"
-    :predefine="['#ffd700', '#409EFF', '#1890ff', '#304156','#212121','#11a983', '#13c2c2', '#6959CD', '#f5222d', ]"
+    :predefine="['#FFB300', '#ffd700', '#409EFF', '#1890ff', '#304156','#212121','#11a983', '#13c2c2', '#6959CD', '#f5222d', ]"
     class="theme-picker"
     popper-class="theme-picker-dropdown"
   />
